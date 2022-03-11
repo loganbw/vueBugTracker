e20e7f393739220ba1b436e7d29eb2f563f8b5dd
Logan W <loganw@IMI-Mac1.local>
2022-03-11 09:41:08 -0500

    first init for vue bug tracker

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<script>
+  import MainHeaderVue from "./components/layouts/MainHeader.vue";
+  export default {
+    components: {
+      MainHeaderVue,
+    },
+  };
+</script>
+
+<template>
+  <section>
+    <MainHeaderVue title="Website Name" />
+  </section>
+</template>
+
+<style>
+  @import "@/assets/base.css";
+
+  /* #app {
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
+  }
+
+
+  @media (hover: hover) {
+    a:hover {
+      background-color: hsla(160, 100%, 37%, 0.2);
+    }
+  }
+
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+  }
+
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav a:first-of-type {
+    border: 0;
+  }
+
+  @media (min-width: 1024px) {
+    body {
+      display: flex;
+      place-items: center;
+    }
+
+    #app {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      padding: 0 2rem;
+    }
+
+
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+
+    nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
+
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
+  } */
+</style>
---
+++ src/components/layouts/MainHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+  <section>
+    <nav></nav>
+  </section>
+</template>
+<script>
+  export default {
+    props: ["title"],
+  };
+</script>
+<style scoped>
+  header {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    background-color: #58004d;
+    color: white;
+    text-align: center;
+    display: flex;
+    place-items: center;
+  }
+</style>
